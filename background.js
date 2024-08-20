6173a823fb1fbb74e363e7d4944e1546ba0e1959
shiftkeys <greenyboy8@gmail.com>
2024-08-20 19:59:03 -0400

    Updated permissions and created functions

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "customSelection",
+        title: "Search wikipedia for '&s'",
+        contexts: ["selection"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((info,tab) => {
+    if(info.menuItemId === "customSelection"){
+        const selected = info.selectionText;
+        const query = encodeURIComponent(selected);
+        const url = 'https://en.wikipedia.org/wiki/{query}';
+        chrome.tabs.create({url:url});
+    }
+});
